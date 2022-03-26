0c61e8337e55d138214666969784b74718844393
Sagnik Som <sagniksom99@gmail.com>
2022-03-26 15:34:13 +0000

    Fixed some minor details to test on arduino mega

--- motor_speed_pwm/motor_speed_pwm.ino
+++ motor_speed_pwm/motor_speed_pwm.ino
@@ -1,12 +1,21 @@
-int[4] speeds = [0,0,0,0]; //left, right, top, bottom
-int[4] enables = [D3,D5,D6,D9];
-int[4] pin1s = [D2,D7,D10,D12];
-int[4] pin2s = [D4,D8,D11,D13];
+int speeds[4] = {0,0,0,0}; //left, right, top, bottom
+
+/*
+int enables[4] = {D3,D5,D6,D9};
+int pin1s[4] = {D2,D7,D10,D12};
+int pin2s[4] = {D4,D8,D11,D13};
+*/
+//////////////////////////////
+//Code added for compiling/testing purposes, comment out if necessary
 int dirs = 0;
+int enables[4] = {6,8,9,12};
+int pin1s[4] = {5,10,13,15};
+int pin2s[4] = {7,8,14,16};
+//////////////////////////////
 
 void setup() {
   // put your setup code here, to run once:
-  for(int i=0;i<4:i++)
+  for(int i=0;i<4;i++)
   {
     pinMode(speeds[i],OUTPUT);
     pinMode(enables[i],OUTPUT);
