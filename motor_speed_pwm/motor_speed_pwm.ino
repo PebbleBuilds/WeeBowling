4910aee37b5bcb6b7b924b8ee156122e1acc2fa4
PebbleBuilds <kiribuilds@gmail.com>
2022-04-06 22:18:34 -0400

    it workgs

--- motor_speed_pwm/motor_speed_pwm.ino
+++ motor_speed_pwm/motor_speed_pwm.ino
@@ -2,9 +2,9 @@ int speeds[4] = {0,0,0,0}; //left, right, top, bottom
 int dirs = 0;
 
 
-int enables[4] = {6,8,9,12};
-int pin1s[4] = {5,10,13,15};
-int pin2s[4] = {7,11,14,16};
+int enables[4] = {3,5,9,10};
+int pin1s[4] = {2,7,11,13};
+int pin2s[4] = {4,8,12,A0};
 
 //////////////////////////////
 //Pinout for mega
@@ -24,6 +24,31 @@ void setup() {
     pinMode(pin1s[i],OUTPUT);
     pinMode(pin2s[i],OUTPUT);
   }
+  
+  // write speeds:
+  for(int i=0;i<4;i++)
+  {
+    if(bitRead(dirs,3-i))
+    {
+      digitalWrite(pin1s[i],HIGH); 
+      digitalWrite(pin2s[i],LOW); 
+
+      Serial.print(pin1s[i]);
+      Serial.print("set to HIGH\n");
+      Serial.print(pin2s[i]);
+      Serial.print("set to LOW\n");
+    }
+    else
+    {
+      digitalWrite(pin1s[i],LOW); 
+      digitalWrite(pin2s[i],HIGH); 
+      Serial.print(pin1s[i]);
+      Serial.print("set to LOW\n");
+      Serial.print(pin2s[i]);
+      Serial.print("set to HIGH\n");
+    }
+    analogWrite(enables[i],speeds[i]);
+  }
 
   Serial.begin(9600);
 }
@@ -37,30 +62,40 @@ void loop() {
       speeds[i] = Serial.read();
     }
     dirs = Serial.read();
-  }
 
-  // write speeds:
-  for(int i=0;i<4;i++)
-  {
-    if(bitRead(dirs,i) == 0)
-    {
-      digitalWrite(pin1s[i],HIGH); 
-      digitalWrite(pin2s[i],LOW); 
-    }
-    else
-    {
-      digitalWrite(pin1s[i],LOW); 
-      digitalWrite(pin2s[i],HIGH); 
-    }
-    analogWrite(enables[i],speeds[i]);
-  }
+    // report status:
+    Serial.print("Speeds:");
+    Serial.print(speeds[0]);
+    Serial.print(speeds[1]);
+    Serial.print(speeds[2]);
+    Serial.print(speeds[3]);
+    Serial.print("Dirs:");
+    Serial.print(dirs);
+    Serial.print("\n");
 
-  // report status:
-  Serial.print("Speeds:");
-  Serial.print(speeds[0]);
-  Serial.print(speeds[1]);
-  Serial.print(speeds[2]);
-  Serial.print(speeds[3]);
-  Serial.print("Dirs:");
-  Serial.print(dirs);
+    // write speeds:
+    for(int i=0;i<4;i++)
+    {
+      if(bitRead(dirs,3-i))
+      {
+        digitalWrite(pin1s[i],HIGH); 
+        digitalWrite(pin2s[i],LOW); 
+
+        Serial.print(pin1s[i]);
+        Serial.print("set to HIGH\n");
+        Serial.print(pin2s[i]);
+        Serial.print("set to LOW\n");
+      }
+      else
+      {
+        digitalWrite(pin1s[i],LOW); 
+        digitalWrite(pin2s[i],HIGH); 
+        Serial.print(pin1s[i]);
+        Serial.print("set to LOW\n");
+        Serial.print(pin2s[i]);
+        Serial.print("set to HIGH\n");
+      }
+      analogWrite(enables[i],speeds[i]);
+    }
+  }
 }
